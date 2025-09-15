c2b1a8cbb072cca8f8c43a022478019e9064fdb3
odewabifd-sys <odewabifd@gmail.com>
2025-09-14 22:17:14 -0700

    Fix: Resolved Supabase and Nav component errors

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
-import Nav from '../components/Nav';;
+import Nav from '../components/Nav';
 
 export default function App({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
